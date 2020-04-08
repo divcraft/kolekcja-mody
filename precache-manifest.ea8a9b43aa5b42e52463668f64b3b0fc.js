c474c1e0ab3337a8bc11d81b27b1ccffc366fd85
divcraft <t.ogonowski@wp.pl>
2020-04-08 11:55:07 +0200

    Updates

---
+++ precache-manifest.ea8a9b43aa5b42e52463668f64b3b0fc.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c85b2f35f35b4458063eb12e57f11aa",
+    "url": "/kolekcja-mody/index.html"
+  },
+  {
+    "revision": "3e99aa677c0ba0e17bd0",
+    "url": "/kolekcja-mody/static/css/main.15a2a572.chunk.css"
+  },
+  {
+    "revision": "fb606fb019d102ed412b",
+    "url": "/kolekcja-mody/static/js/2.a9f9661d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kolekcja-mody/static/js/2.a9f9661d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e99aa677c0ba0e17bd0",
+    "url": "/kolekcja-mody/static/js/main.49f615f9.chunk.js"
+  },
+  {
+    "revision": "7ebc0002dd0a57e8be94",
+    "url": "/kolekcja-mody/static/js/runtime-main.4244f501.js"
+  },
+  {
+    "revision": "3faa8e0d1a20d895691e24fa020d7503",
+    "url": "/kolekcja-mody/static/media/adidas-logo.3faa8e0d.png"
+  },
+  {
+    "revision": "1ba5b7bbacaa517ae69adaf978c58df4",
+    "url": "/kolekcja-mody/static/media/arrow.1ba5b7bb.svg"
+  },
+  {
+    "revision": "6d1aa9be823b8b7895a09f566fadb5ad",
+    "url": "/kolekcja-mody/static/media/converse-logo.6d1aa9be.png"
+  },
+  {
+    "revision": "909b2675746576437bee6676f7ac2fc0",
+    "url": "/kolekcja-mody/static/media/delivery-img-1366.909b2675.jpg"
+  },
+  {
+    "revision": "53230c78a4668e2e9de01f2271674d68",
+    "url": "/kolekcja-mody/static/media/delivery-img-1920.53230c78.jpg"
+  },
+  {
+    "revision": "2522c3a91d45ca917d6271cdfae239ec",
+    "url": "/kolekcja-mody/static/media/delivery-img-768.2522c3a9.jpg"
+  },
+  {
+    "revision": "e05d14af0fb9a541765548c43e8feb73",
+    "url": "/kolekcja-mody/static/media/form-img-1366.e05d14af.jpg"
+  },
+  {
+    "revision": "ca27581a516dfd23664e8a50a109c67c",
+    "url": "/kolekcja-mody/static/media/form-img-1920.ca27581a.jpg"
+  },
+  {
+    "revision": "5a3a2f2fa67d1d1f914e5111a00661cc",
+    "url": "/kolekcja-mody/static/media/form-img-768.5a3a2f2f.jpg"
+  },
+  {
+    "revision": "e10dfd4d393a9b5236cf66a48a1ff1a1",
+    "url": "/kolekcja-mody/static/media/girl-967.e10dfd4d.png"
+  },
+  {
+    "revision": "49aa44dce555e968f8570942e0a037aa",
+    "url": "/kolekcja-mody/static/media/hm-logo.49aa44dc.png"
+  },
+  {
+    "revision": "75e08320b9a2d428cc63eb0dac83cacb",
+    "url": "/kolekcja-mody/static/media/home-bg-1366.75e08320.jpg"
+  },
+  {
+    "revision": "1797f43524a85f79597eec1d610b595b",
+    "url": "/kolekcja-mody/static/media/home-bg-1920.1797f435.jpg"
+  },
+  {
+    "revision": "120c10d20498963e15827082c3498c00",
+    "url": "/kolekcja-mody/static/media/home-bg-768.120c10d2.jpg"
+  },
+  {
+    "revision": "842eb8ea0090c1a32b6b8fb6438fd017",
+    "url": "/kolekcja-mody/static/media/hugo-boss-logo.842eb8ea.png"
+  },
+  {
+    "revision": "65dcc3e8a75cf2d06e7879f85174ce5b",
+    "url": "/kolekcja-mody/static/media/levis-logo.65dcc3e8.png"
+  },
+  {
+    "revision": "9d24164d70a05c139f3f49b65369dadd",
+    "url": "/kolekcja-mody/static/media/media-fb.9d24164d.svg"
+  },
+  {
+    "revision": "fb58995ca707def6cc5d45ef1a3c721d",
+    "url": "/kolekcja-mody/static/media/media-gplus.fb58995c.svg"
+  },
+  {
+    "revision": "a803fa4937ea6606a31188e6e18efddb",
+    "url": "/kolekcja-mody/static/media/media-ig.a803fa49.svg"
+  },
+  {
+    "revision": "896e241a97e0a1eabe0729d61692c641",
+    "url": "/kolekcja-mody/static/media/menu-icon-gray.896e241a.svg"
+  },
+  {
+    "revision": "2b7911b6ab8f9096033b05a7b26ad007",
+    "url": "/kolekcja-mody/static/media/menu-icon-white.2b7911b6.svg"
+  },
+  {
+    "revision": "22de59155a3e2fb51fe0510cfe6d906c",
+    "url": "/kolekcja-mody/static/media/new-balance-logo.22de5915.png"
+  },
+  {
+    "revision": "ad8ef4b8661f5ceb7a8faa212761d9cb",
+    "url": "/kolekcja-mody/static/media/news-img1-831.ad8ef4b8.jpg"
+  },
+  {
+    "revision": "f1aeaba98de90292478c3199b156e30a",
+    "url": "/kolekcja-mody/static/media/pick-baby.f1aeaba9.svg"
+  },
+  {
+    "revision": "155934621f0f728f474ff152677c6b02",
+    "url": "/kolekcja-mody/static/media/pick-man.15593462.svg"
+  },
+  {
+    "revision": "36e71874f468b8e9a1d6cdd3574d5596",
+    "url": "/kolekcja-mody/static/media/pick-woman.36e71874.svg"
+  },
+  {
+    "revision": "366dd8ec4fda229677a711c28fec812b",
+    "url": "/kolekcja-mody/static/media/reebok-logo.366dd8ec.png"
+  },
+  {
+    "revision": "7b64f8791022944176f1a528dd9ac255",
+    "url": "/kolekcja-mody/static/media/zara-logo.7b64f879.png"
+  }
+]);
